9fcfa0ee294394ce2f80fa4ac1e8906515392009
Danilo Bruno <danilobsilv@gmail.com>
2024-01-13 14:24:33 -0400

    add: getAllUser route

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,5 +3,6 @@ const db = require("../data/repository/dbConnection");
 const userController = require("../rules/controllers/userController");
 
 router.route("/").post((req, res) => userController.createUser(req, res, db));
+router.route("/").get((req, res) => userController.getAllUsers(req, res, db));
 
 module.exports = router;
