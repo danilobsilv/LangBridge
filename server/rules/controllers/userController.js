5d6f61ee79653cc5e011a6fbcfddb743a87dc8d9
Danilo Bruno <danilobsilv@gmail.com>
2024-01-13 13:45:13 -0400

    add: cookie storage in client side

--- server/rules/controllers/userController.js
+++ server/rules/controllers/userController.js
@@ -30,77 +30,79 @@ const validateParams = (params) => {
 const generateToken = (username, secret) =>{
   const tokenPayload = {
     username,
-    role: "user"
-  }
-
-  const options = {
-    expiresIn: '3h',
+    role: 'user',
   };
 
-  return jwt.sign(tokenPayload,  secret, options);
-}
+  return jwt.sign(tokenPayload, secret);
+};
 
 const userController = {
-      createUser: async (req, res, db) => {
-        try {
-          const {fullName, username, email, password, preferredLanguage, birthDate} = req.body;
-    
-          const isValidParams = validateParams({fullName, username, email, password, preferredLanguage, birthDate});
-          const isValidEmail = checkEmailFormat(email);
-          const isValidAge = checkUnder100YearsOld(birthDate);
-    
-          if (!isValidParams) {
-            return res.status(400).send({Error: 'Missing required parameters!'});
-          }
-    
-          if (!isValidEmail) {
-            console.error('Formato de email inválido');
-            return res.status(400).send({Error: 'Invalid email format!'});
-          }
-    
-          if (!isValidAge) {
-            console.error('Passou de 100 anos de idade');
-            return res.status(400).send({Error: 'Age exceeded 100 years!'});
-          }
-    
-          const userEmailExistsQuery = `SELECT * FROM User WHERE email = ?`;
-          db.get(userEmailExistsQuery, email, (error, row) => {
-            if (error) {
-              console.error('Error when checking the user email: ', error.message);
-              res.status(500).json({ Error: 'Error checking the user email!' });
-              return;
-            }
-          
-            if (row) {
-              console.error('Email já existe');
-              return res.status(400).send({ error: 'Email already exists!' });
-            }
-            
-            const hashedPassword = bcrypt.hash(password, 10);
-            const insertQuery = `
+  createUser: async (req, res, db) => {
+    try {
+      const {fullName, username, email, password, preferredLanguage, birthDate} = req.body;
+
+      const isValidParams = validateParams({fullName, username, email, password, preferredLanguage, birthDate});
+      const isValidEmail = checkEmailFormat(email);
+      const isValidAge = checkUnder100YearsOld(birthDate);
+
+      if (!isValidParams) {
+        return res.status(400).send({Error: 'Missing required parameters!'});
+      }
+
+      if (!isValidEmail) {
+        console.error('Formato de email inválido');
+        return res.status(400).send({Error: 'Invalid email format!'});
+      }
+
+      if (!isValidAge) {
+        console.error('Passou de 100 anos de idade');
+        return res.status(400).send({Error: 'Age exceeded 100 years!'});
+      }
+
+      const userEmailExistsQuery = `SELECT * FROM User WHERE email = ?`;
+      db.get(userEmailExistsQuery, email, (error, row) => {
+        if (error) {
+          console.error('Error when checking the user email: ', error.message);
+          res.status(500).json({Error: 'Error checking the user email!'});
+          return;
+        }
+
+        if (row) {
+          console.error('Email já existe');
+          return res.status(400).send({error: 'Email already exists!'});
+        }
+
+        const hashedPassword = bcrypt.hash(password, 10);
+        const insertQuery = `
               INSERT INTO User (fullName, username, email, password, preferredLanguage, birthDate)
               VALUES (?, ?, ?, ?, ?, ?)
               `;
-            const params = [fullName, username, email, hashedPassword, preferredLanguage, birthDate];
-            db.run(insertQuery, params, function(error) {
-              if (error) {
-                console.error('Error when creating the user: ', error.message);
-                res.status(500).json({Error: 'Error creating the user!'});
-                return;
-              }
-              const userId = this.lastID;
-              
-              const token = generateToken(username, jwtSecret);
-              res.status(201).json({message: 'User successfully created!', userId, token: token});
-              console.log('User successfully created. ID: ', userId);
-            });
-          });
-        } catch (error) {
-          console.log('ERROR:', error);
-          res.status(500).json({Error: 'Error creating user ' + error.message});
-        }
-      },
-    };
-    
+        const params = [fullName, username, email, hashedPassword, preferredLanguage, birthDate];
+        db.run(insertQuery, params, function(error) {
+          if (error) {
+            console.error('Error when creating the user: ', error.message);
+            res.status(500).json({Error: 'Error creating the user!'});
+            return;
+          }
+          const userId = this.lastID;
 
-module.exports = userController
+          const token = generateToken(username, jwtSecret);
+
+          res.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 3 * 60 * 60 * 1000,
+          });
+
+          res.status(201).json({message: 'User successfully created!', userId: userId, token: token});
+          console.log('User successfully created. ID: ', userId);
+        });
+      });
+    } catch (error) {
+      console.log('ERROR:', error);
+      res.status(500).json({Error: 'Error creating user ' + error.message});
+    }
+  },
+};
+
+
+module.exports = userController;
