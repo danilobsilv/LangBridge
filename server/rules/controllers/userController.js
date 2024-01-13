9ceb567c87bc7577fb5b4ee090e8301dcbd93d93
Danilo Bruno <danilobsilv@gmail.com>
2024-01-12 22:47:09 -0400

    to fix: jwt token

--- server/rules/controllers/userController.js
+++ server/rules/controllers/userController.js
@@ -12,7 +12,7 @@ const checkEmailFormat = (email) => {
 const checkUnder100YearsOld = (birthdate) => {
   const currentDate = new Date();
 
-  // Reformat the date if it's in the "DD/MM/YYYY" format
+  // p aceitar diferentes tipos de entrada
   if (/^\d{2}\/\d{2}\/\d{4}$/.test(birthdate)) {
     birthdate = birthdate.split('/').reverse().join('-');
   }
@@ -23,88 +23,70 @@ const checkUnder100YearsOld = (birthdate) => {
   return ageDifference < 100;
 };
 
-
-// const userEmailExists = (email) => {
-//       userEmailExistsQuery = `SELECT * FROM User WHERE email = ?`;
-
-//       db.run(userEmailExistsQuery, email, (error, row) => {
-//             if (error) {
-//               console.error('Error when checking the user email: ', error.message);
-//               res.status(500).json({ Error: 'Error checking the user email.' });
-//               return;
-//             }
-          
-//             if (row) {
-//               console.error('Email already exists.');
-//               return res.status(400).send({ error: 'Email already exists.' });
-//             }
-          
-//             // Continue with user creation if the email does not exist...
-//           });
-// }
-
 const validateParams = (params) => {
   return Object.values(params).every((param) => param !== undefined && param !== '');
 };
 
 const userController = {
-  createUser: async (req, res, db) => {
-    try {
-      const {fullName, username, email, password, preferredLanguage, birthDate} = req.body;
-
-      const isValidParams = validateParams({fullName, username, email, password, preferredLanguage, birthDate});
-      const isValidEmail = checkEmailFormat(email);
-      const isValidAge = checkUnder100YearsOld(birthDate);
-
-      if (!isValidParams) {
-        return res.status(400).send({Error: 'Missing required parameters!'});
-      }
-
-      if (!isValidEmail) {
-        console.error('Formato de email inválido');
-        return res.status(400).send({Error: 'Invalid email format!'});
-      }
-
-      if (!isValidAge) {
-        console.error('Passou de 100 anos de idade');
-        return res.status(400).send({Error: 'Age exceeded 100 years!'});
-      }
-
-      // const userEmailExistsQuery = `SELECT * FROM User WHERE email = ?`;
-      // db.run(userEmailExistsQuery, email, (error, row) => {
-      //       if (error) {
-      //         console.error('Error when checking the user email: ', error.message);
-      //         res.status(500).json({ Error: 'Error checking the user email.' });
-      //         return;
-      //       }
+      createUser: async (req, res, db) => {
+        try {
+          const {fullName, username, email, password, preferredLanguage, birthDate} = req.body;
+    
+          const isValidParams = validateParams({fullName, username, email, password, preferredLanguage, birthDate});
+          const isValidEmail = checkEmailFormat(email);
+          const isValidAge = checkUnder100YearsOld(birthDate);
+    
+          if (!isValidParams) {
+            return res.status(400).send({Error: 'Missing required parameters!'});
+          }
+    
+          if (!isValidEmail) {
+            console.error('Formato de email inválido');
+            return res.status(400).send({Error: 'Invalid email format!'});
+          }
+    
+          if (!isValidAge) {
+            console.error('Passou de 100 anos de idade');
+            return res.status(400).send({Error: 'Age exceeded 100 years!'});
+          }
+    
+          const userEmailExistsQuery = `SELECT * FROM User WHERE email = ?`;
+          db.get(userEmailExistsQuery, email, (error, row) => {
+            if (error) {
+              console.error('Error when checking the user email: ', error.message);
+              res.status(500).json({ Error: 'Error checking the user email!' });
+              return;
+            }
           
-      //       if (row) {
-      //         console.error('Email already exists.');
-      //         return res.status(400).send({ error: 'Email already exists.' });
-      //       }
-          
-      //     });
-
-      const insertQuery = `
-          INSERT INTO User (fullName, username, email, password, preferredLanguage, birthDate)
-          VALUES (?, ?, ?, ?, ?, ?)
-          `;
-      const params = [fullName, username, email, password, preferredLanguage, birthDate];
-      db.run(insertQuery, params, function(error) {
-        if (error) {
-          console.error('Error when creating the user: ', error.message);
-          res.status(500).json({Error: 'Error creating the user.'});
-          return;
+            if (row) {
+              console.error('Email já existe');
+              return res.status(400).send({ error: 'Email already exists!' });
+            }
+            
+            const hashedPassword = bcrypt.hash(password, 10);
+            const insertQuery = `
+              INSERT INTO User (fullName, username, email, password, preferredLanguage, birthDate)
+              VALUES (?, ?, ?, ?, ?, ?)
+              `;
+            const params = [fullName, username, email, hashedPassword, preferredLanguage, birthDate];
+            db.run(insertQuery, params, function(error) {
+              if (error) {
+                console.error('Error when creating the user: ', error.message);
+                res.status(500).json({Error: 'Error creating the user!'});
+                return;
+              }
+              const userId = this.lastID;
+              const token = jwt.sign({ username: username }, process.env.JWT_SECRET);
+              res.status(201).json({message: 'User successfully created!', userId, token: token});
+              console.log('User successfully created. ID: ', userId);
+            });
+          });
+        } catch (error) {
+          console.log('ERROR:', error);
+          res.status(500).json({Error: 'Error creating user ' + error.message});
         }
-        const userId = this.lastID;
-        res.status(201).json({message: 'User successfully created.', userId});
-        console.log('User successfully created. ID: ', userId);
-      });
-    } catch (error) {
-      console.log('ERROR:', error);
-      res.status(500).json({Error: 'Error creating user.'});
-    }
-  },
-};
+      },
+    };
+    
 
 module.exports = userController;
