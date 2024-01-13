896139d57f705a326123538615c28c6b6b526028
Danilo Bruno <danilobsilv@gmail.com>
2024-01-13 15:23:12 -0400

    add: getUserById route

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 const db = require("../data/repository/dbConnection");
 const userController = require("../rules/controllers/userController");
 
-router.route("/").post((req, res) => userController.createUser(req, res, db));
-router.route("/").get((req, res) => userController.getAllUsers(req, res, db));
-router.route("/:userId").get((req, res) => userController.getUserById(req, res, db));
+router.route("/")
+      .post((req, res) => userController.createUser(req, res, db))
+      .get((req, res) => userController.getAllUsers(req, res, db));
+
+
+router.route("/:userId")
+      .get((req, res) => userController.getUserById(req, res, db));
 
 module.exports = router;
