38717b8551ae070205b01f6f32cd6935e430d3ee
Danilo Bruno <danilobsilv@gmail.com>
2024-01-15 19:23:42 -0400

    add: deleteUserById

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const db = require("../data/databaseCode/dbConnection");
-const userRepository = require("../rules/repository/userRepository");
+const userController = require("../rules/controllers/userController");
 
 router.route("/")
-      .post((req, res) => userRepository.createUser(req, res, db))
-      .get((req, res) => userRepository.getAllUsers(req, res, db));
+      .post((req, res) => userController.createUser(req, res, db))
+      .get((req, res) => userController.getAllUsers(req, res, db));
 
 
 router.route("/:userId")
-      .get((req, res) => userRepository.getUserById(req, res, db))
-      .put((req, res) => userRepository.updateUserById(req, res, db))
-      .delete((req, res) => userRepository.deleteUserById(req, res, db));
+      .get((req, res) => userController.getUserById(req, res, db))
+      .put((req, res) => userController.updateUserById(req, res, db))
+      .delete((req, res) => userController.deleteUserById(req, res, db));
 
 module.exports = router;
