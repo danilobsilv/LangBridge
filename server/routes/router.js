75e24283c16c77a732296386346a0ee7fec2af1c
Danilo Bruno <danilobsilv@gmail.com>
2024-01-15 21:51:55 -0400

    add: translationRequestsRouter

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
-// Users router
+// Users Router
 const userRouter = require("./userRouter");
 router.use("/user", userRouter);
+
+// Translation Requests Router
+const tranlationRequetsRouter = require("./translationRequestRouter");
+router.use("/translation-request", tranlationRequetsRouter);
 
 module.exports = router;
