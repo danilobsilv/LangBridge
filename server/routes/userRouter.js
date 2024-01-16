fb3cc6d920068db870f1075d300d6f596d6c3b85
Danilo Bruno <danilobsilv@gmail.com>
2024-01-15 21:49:28 -0400

    change: const name -- 'db' to 'database'

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
-const db = require("../data/databaseCode/dbConnection");
+const database = require("../data/databaseCode/dbConnection");
 const userController = require("../rules/controllers/userController");
 
 router.route("/")
-      .post((req, res) => userController.createUser(req, res, db))
-      .get((req, res) => userController.getAllUsers(req, res, db));
+      .post((req, res) => userController.createUser(req, res, database))
+      .get((req, res) => userController.getAllUsers(req, res, database));
 
 
 router.route("/:userId")
-      .get((req, res) => userController.getUserById(req, res, db))
-      .put((req, res) => userController.updateUserById(req, res, db))
-      .delete((req, res) => userController.deleteUserById(req, res, db));
+      .get((req, res) => userController.getUserById(req, res, database))
+      .put((req, res) => userController.updateUserById(req, res, database))
+      .delete((req, res) => userController.deleteUserById(req, res, database));
 
 module.exports = router;
