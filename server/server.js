6f36457666cc7662fdf56628ba6ad7a0fe0af979
Danilo Bruno <danilobsilv@gmail.com>
2024-01-28 20:18:28 -0400

    add: errorHandler midware

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const routes = require('./routes/router');
-const errorHandler = require('./middleware/errorHandler');
+const { errorHandler } = require('../server/middleware/errorHandler');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 
