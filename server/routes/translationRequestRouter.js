06c9654baf8b88236a471f8d8160515ad1eec6ad
Danilo Bruno <danilobsilv@gmail.com>
2024-02-10 16:57:15 -0400

    router

---
+++ server/routes/translationRequestRouter.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const database = require("../data/databaseCode/dbConnection");
+const translationRequestController = require('../rules/controllers/translationRequestController');
+
+router.route("/:userId/:textId").post((req, res, next) => translationRequestController.createTranslationRequest(req, res, next, database));
+
+router.route("/").get((req, res, next) => translationRequestController.getAllTranslationRequests(req, res, next, database));
+
+router.route("/:requestId").get((req, res, next) => translationRequestController.getTranslationRequestById(req, res, next, database));
+
+module.exports = router;
