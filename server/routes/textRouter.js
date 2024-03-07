b2cd0b794371121b76446e49f998d3eca8516291
Danilo Bruno <danilobsilv@gmail.com>
2024-03-06 22:03:08 -0400

    remove: user id and request id from the post  route

--- server/routes/textRouter.js
+++ server/routes/textRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const database = require("../data/databaseCode/dbConnection");
 const textController = require("../rules/controllers/textController");
 
-router.route("/:userId/:translationRequestId")
+router.route("/")
       .post((req, res, next) => textController.createText(req, res, database, next));
 
 router.route("/").get((req, res, next) => textController.getAllText(req, res, database, next));
